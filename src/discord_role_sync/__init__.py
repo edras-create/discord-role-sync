d8c7e09ce99c30679978f08ddec8ba67058a6b3e
edras-create <73792597+edras-create@users.noreply.github.com>
2026-08-15 08:29:25 -0600

    Add src/discord_role_sync/__init__.py

---
+++ src/discord_role_sync/__init__.py
@@ -0,0 +1,6 @@
+"""One-shot Discord role synchronization package."""
+
+from .models import Action, SourceMode, SyncSummary
+
+__all__ = ["Action", "SourceMode", "SyncSummary"]
+__version__ = "1.0.0"
